5917179850b78625b24bb9c26edff194b40d1815
mgenius3 <benmos16@gmail.com>
2024-07-12 12:27:20 +0000

    clean up

--- src/components/musiclabel/index.jsx
+++ src/components/musiclabel/index.jsx
@@ -59,7 +59,7 @@ export default function MusicLabel() {
       <WhoWeAre />
       <OurServices />
       <WhoWeAreMusicLabel />
-      <Records />
+      {/* <Records /> */}
       <AboutUs />
       <Consultation />
       <Whatcanwedoforyou />
